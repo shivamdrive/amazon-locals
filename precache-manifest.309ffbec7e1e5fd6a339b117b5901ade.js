6248a5f5a5f4bcbda25335e688c3634ff247c291
shivamdrive <shivamdrive1@gmail.com>
2023-07-09 23:03:05 +0530

    Updates

---
+++ precache-manifest.309ffbec7e1e5fd6a339b117b5901ade.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437bd663577a3d81a898ae9dbce4e055",
+    "url": "/amazon-locals/index.html"
+  },
+  {
+    "revision": "9dd0f407fb386280bf0a",
+    "url": "/amazon-locals/static/css/2.587a2a2e.chunk.css"
+  },
+  {
+    "revision": "97f409c79ec1b5ae70ca",
+    "url": "/amazon-locals/static/css/main.1d7cff6b.chunk.css"
+  },
+  {
+    "revision": "9dd0f407fb386280bf0a",
+    "url": "/amazon-locals/static/js/2.e7101dc9.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/amazon-locals/static/js/2.e7101dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f409c79ec1b5ae70ca",
+    "url": "/amazon-locals/static/js/main.838521fa.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/amazon-locals/static/js/main.838521fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b992be4bcfc25df6a4a3",
+    "url": "/amazon-locals/static/js/runtime-main.57b1aa1c.js"
+  }
+]);
